2303aa829f8a2564e4eff0170f6e0a86f48ce609
Yeswanth S <yeswanth1632001@gmail.com>
2024-05-07 10:47:21 +0530

    Time: 2 ms (63.35%), Space: 45.7 MB (74.84%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,28 +1,29 @@
 class Solution {
+
+    public int[] mergeSort(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+
+        int[] arr = new int[n1+n2];
+
+        int i=0,j=0,k=0;
+        while(i < n1 && j < n2) {
+            if(nums1[i] < nums2[j]) arr[k++] = nums1[i++];
+            else arr[k++] = nums2[j++];
+        }
+
+        while(i < n1) arr[k++] = nums1[i++];
+        while(j < n2) arr[k++] = nums2[j++];
+        return arr;
+    }
+
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] arr = new int[nums1.length + nums2.length];
+        int[] arr = mergeSort(nums1, nums2);
 
-        int i = 0,j = 0,k = 0;
-        while(i < nums1.length && j < nums2.length) {
-            if(nums1[i] < nums2[j]) {
-                arr[k++] = nums1[i++];
-            } else {
-                arr[k++] = nums2[j++];
-            }
+        if(arr.length%2 != 0) {
+            return arr[arr.length/2];
         }
 
-        while(i < nums1.length) {
-            arr[k++] = nums1[i++];
-        }
-
-        while(j < nums2.length) {
-            arr[k++] = nums2[j++];
-        }
-
-        if(arr.length%2 == 0) {
-            return ( arr[arr.length/2] + arr[arr.length/2 - 1] )/2d;
-        } else {
-            return arr[arr.length/2]+0.0;
-        }
+        return (arr[arr.length/2] + arr[(arr.length/2)-1])/2d;
     }
 }
